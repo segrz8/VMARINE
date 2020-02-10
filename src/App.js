aa13b213cbaceeab4575ad3270968cbb2311a041
Sebastian Grzelak <segrz8@gmail.com>
2020-02-10 18:28:57 +0100

    Complete 'Main'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import Header from './Header';
 import Hamburger from './Hamburger';
