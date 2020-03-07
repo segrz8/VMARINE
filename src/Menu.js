8ad101841857efa1ea1b37f7cba7c5b126d19aa5
Sebastian Grzelak <segrz8@gmail.com>
2020-03-07 19:49:57 +0100

    Smaller menu

--- src/Menu.js
+++ src/Menu.js
@@ -12,7 +12,7 @@ const Menu = (props) => {
             <ul>
                 <li><Link to='/'>O nas</Link></li>
                 <li><Link to='/dockmate'>Dockmate - bezprzewodowe systemy sterowania jachtami</Link></li>
-                <li><Link to='/havospark'>SKUTERY PODWODNE HAVOSPARK AQUAJET H2</Link></li>
+                <li><Link to='/havospark'>Skutery podwodne Havoslark Aquaject H2</Link></li>
                 <li><Link to='/fendertex'>Lekkie i wytrzymałe odbijacze Fendertex</Link></li>
                 <li><Link to='/rescue'>Rescue Ark H3 - zdalne koło ratunkowe</Link></li>
                 <li><Link to='/yachts'>Jachty na sprzedaż</Link></li>
